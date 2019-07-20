56f53628c41250d45b628240606b2b43ae32b57b
Naiyer Asif <naiyer.app@gmail.com>
2019-07-21 02:30:52 +0530

    Tweaks on search component

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -19,7 +19,7 @@
         ref="search"
       >
       <div class="absolute top-0 ml-3" style="top:10px">
-        <svg fill="currentColor" class="text-copy-primary h-6 w-6" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>
+        <svg fill="currentColor" class="text-copy-primary h-6 w-6" viewBox="0 0 24 24" width="24" height="24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>
       </div>
       <div
         v-if="query.length > 0"
@@ -27,18 +27,18 @@
         style="top:2px;"
         @click="reset"
       >
-        &times;
+        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="inline"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
       </div>
     </div>
     <transition name="fade">
-      <div v-if="query.length > 0 && searchResultsVisible" class="normal-case absolute border left-0 right-0 w-108 text-left mb-4 mt-2 rounded-lg shadow overflow-hidden z-10 overflow-y-auto" style="max-height: 32rem">
+      <div v-if="query.length > 0 && searchResultsVisible" class="normal-case absolute border left-0 right-0 w-108 text-left mb-4 mt-2 rounded-lg shadow sm:shadow md:shadow-md lg:shadow-lg xl:shadow-xl overflow-hidden z-10 overflow-y-auto" style="max-height: 32rem">
         <div class="flex flex-col" ref="results">
           <a
             v-for="(post, index) in results"
             :key="index"
             :href="post.item.path"
             @click="reset"
-            class="bg-background-quaternary border-b border-gray-400 text-xl cursor-pointer p-4 search-hover"
+            class="bg-background-quaternary font-semibold border-b border-gray-400 text-xl cursor-pointer p-4 search-hover"
             :class="{ 'search-highlighted' : index === highlightedIndex }"
           >
             {{ post.item.title }}
