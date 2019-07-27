17888bf5a8389025d74a8db339b23f8abdc5b011
Microflash <naiyer.app@gmail.com>
2019-07-27 13:18:32 +0530

    Setup new RSS feed with full content

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -54,7 +54,7 @@
         </div>
         <ul class="flex items-center">
           <li class="mr-8">
-            <a href="rss.xml" title="RSS" class="text-copy-tertiary hover:text-gray-600">
+            <a href="feed.xml" title="RSS" class="text-copy-tertiary hover:text-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 11a9 9 0 0 1 9 9" /><path d="M4 4a16 16 0 0 1 16 16" /><circle cx="5" cy="19" r="1" /></svg>
             </a>
           </li>
