baa9f0a8fe0f11c65570b1a148c61b757bdb08ac
Microflash <naiyer.app@gmail.com>
2019-09-19 01:08:44 +0530

    Fix label for search

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -16,7 +16,7 @@
         @keyup.enter="gotoLink"
         @blur="searchResultsVisible = false"
         @focus="searchResultsVisible = true"
-        ref="search" aria-labelledby="Search"
+        ref="search" aria-label="Search"
       >
       <div class="absolute top-0 ml-3" style="top:8px">
         <svg fill="currentColor" class="text-content-header h-6 w-6" viewBox="0 0 24 24" width="24" height="24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>
