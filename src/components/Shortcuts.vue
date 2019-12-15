ad9073cec089aab0dcde0ba744b6b04f711ac255
Microflash <naiyer.app@gmail.com>
2019-12-15 11:39:33 +0530

    feat(ux): duotone quicklink icons

--- src/components/Shortcuts.vue
+++ src/components/Shortcuts.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="shortcuts">
     <a href="#" title="Back to top" v-scroll-to="scrollPath">
-      <svg class="icon icon-up" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="16 12 12 8 8 12"></polyline><line x1="12" y1="16" x2="12" y2="8"></line></svg>
+      <svg class="icon icon-up" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10" fill="none"/><circle cx="12" cy="12" r="10" fill="currentColor" opacity=".2"/><polyline points="16 12 12 8 8 12" fill="none"/><line x1="12" y1="16" x2="12" y2="8" fill="none"/></svg>
     </a>
     <a href="/" title="Home">
-      <svg class="icon icon-home" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+      <svg class="icon icon-home" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" fill="none"/><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" fill="currentColor" opacity=".2"/><polyline points="9 22 9 12 15 12 15 22" fill="none"/></svg>
     </a>
   </div>
 </template>
--- src/pages/about/Naiyer.vue
+++ src/pages/about/Naiyer.vue
@@ -126,7 +126,7 @@
     </div>
     <div class="to-the-top">
       <a href="#" title="Back to top" v-scroll-to="'#about-me'">
-        <svg class="icon icon-up" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="16 12 12 8 8 12"></polyline><line x1="12" y1="16" x2="12" y2="8"></line></svg>
+        <svg class="icon icon-up" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10" fill="none"/><circle cx="12" cy="12" r="10" fill="currentColor" opacity=".2"/><polyline points="16 12 12 8 8 12" fill="none"/><line x1="12" y1="16" x2="12" y2="8" fill="none"/></svg>
       </a>
     </div>
   </Layout>
