a3b4a2d724de36f1ddb0a8a8829cd911ddbe557a
Naiyer Asif <naiyer.app@gmail.com>
2020-10-05 17:30:02 +0530

    fix: add report generation as preprocessor step

--- app.server.js
+++ app.server.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const fs = require('fs')
 const dotenv = require('dotenv')
 const { google } = require('googleapis')
 
@@ -27,6 +29,23 @@ async function getReport() {
   })
 }
 
+function writeToFile(target, output, data) {
+  const outputPath = path.resolve(process.cwd(), output.dir)
+  const outputPathExists = fs.existsSync(outputPath)
+  const fileName = output.name.endsWith('.json') ? output.name : `${output.name}.json`
+
+  if (!outputPathExists) {
+    fs.mkdirSync(outputPath)
+  }
+
+  if (outputPathExists && data && target) {
+    console.log(`Generate ${target}`)
+  }
+
+  fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(data))
+}
+
 module.exports = {
-  getReport
+  getReport,
+  writeToFile
 }
