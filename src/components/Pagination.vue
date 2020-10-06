1262539a0bcd9228076b1a182b984e7282524961
Naiyer Asif <naiyer.app@gmail.com>
2020-10-06 21:17:42 +0530

    feat(ux): rework pagination component phase 1

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="sidekick">
     <div class="sidekick-wrapper">
-      <div class="topics" v-if="input.totalPages > 1">
-        <g-link class="is-prev" :to="previousPage(input.currentPage)" v-if="input.currentPage > 1">&larr; Prev</g-link>
-        <span class="is-mid">page {{ input.currentPage }} of {{ input.totalPages }}</span>
-        <g-link class="is-next" :to="nextPage(input.currentPage)" v-if="input.currentPage < input.totalPages">Next &rarr;</g-link>
+      <div class="pagination" v-if="input.totalPages > 1 && input.totalPages < 6">
+        <g-link class="is-prev" :to="previousPage" v-if="input.currentPage > 1">&larr;</g-link>
+        <g-link :class="{ 'is-current': input.currentPage == idx }" v-for="idx in input.totalPages" :key="idx" :to="destination(idx)">{{ idx }}</g-link>
+        <g-link class="is-next" :to="nextPage" v-if="input.currentPage < input.totalPages">&rarr;</g-link>
       </div>
-      <div class="topics" v-else>
+      <div class="pagination" v-else>
         <div class="is-muted">🔚 end of the list</div>
       </div>
     </div>
@@ -19,14 +19,20 @@ export default {
   computed: {
     base() {
       return this.path || ''
+    },
+    previousPage() {
+      const currentPage = this.input.currentPage
+      return [0, 1].includes(currentPage - 1) ? `${this.base}/` : `${this.base}/${currentPage - 1}/`
+    },
+    nextPage() {
+      const currentPage = this.input.currentPage
+      const totalPages = this.input.totalPages
+      return totalPages > currentPage ? `${this.base}/${currentPage + 1}/` : `${this.base}/${currentPage}/`
     }
   },
   methods: {
-    previousPage(currentPage) {
-      return [0, 1].includes(currentPage - 1) ? `${this.base}/` : `${this.base}/${currentPage - 1}/`;
-    },
-    nextPage(currentPage) {
-      return this.input.totalPages > currentPage ? `${this.base}/${currentPage + 1}/` : `${this.base}/${currentPage}/`;
+    destination(index) {
+      return [0, 1].includes(index) ? `${this.base}/` : `${this.base}/${index}/`
     }
   }
 }
