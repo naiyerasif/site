e54640bc2d9e348f7c688cf62023b502fc418937
Naiyer Asif <naiyer.app@gmail.com>
2020-10-18 15:17:45 +0530

    refactor(dev): stop preprocessing grid

--- src/layouts/partials/Footer.vue
+++ src/layouts/partials/Footer.vue
@@ -3,7 +3,7 @@
     <p class="copyright">
       <g-link to="/">Microflash</g-link> is created, written, and maintained by <g-link to="/profile/naiyer/">Naiyer Asif</g-link>. It's built using <a href="/profile/naiyer/#technologies">Jamstack</a> and deployed on <a target="_blank" rel="noopener noreferrer" href="https://netlify.com/">Netlify</a> with <a target="_blank" rel="noopener noreferrer" href="https://github.com/Microflash/site/actions">GitHub Actions</a>.
     </p>
-    <div class="footer-grid">
+    <div class="grid">
       <nav class="footer-menu">
         <div class="footer-title">Resources</div>
         <a href="/feed.xml"><Sprite symbol="icon-rss" class="icon-sm gap-ch" />RSS feed</a>
