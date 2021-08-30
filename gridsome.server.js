5fce32c3e0adcc3b6b77c4da1e302fd309ddd30e
Naiyer Asif <naiyer.app@gmail.com>
2021-08-30 11:55:21 +0530

    refactor: search index generation

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,69 +1,60 @@
-const { resolveBlog } = require('./lib/typeResolver')
-const { writeToFile } = require('./app.server')
-const { paths } = require('./app.config')
+const { resolveNode } = require('./lib/nodeResolver')
+const { createSearchIndex } = require('./lib/searchIndex')
 const projects = require('./content/projects')
 
 module.exports = api => {
-
-  api.onCreateNode(options => {
-    resolveBlog(options)
-  })
+  api.onCreateNode(options => resolveNode(options))
 
   api.loadSource(async ({ addCollection }) => {
     const allProjects = addCollection({
-      typeName: 'Project'
+      typeName: 'Project',
     })
     projects.forEach(project => {
       allProjects.addNode({
         title: project.title,
         description: project.description,
-        link: project.path
+        link: project.path,
       })
     })
   })
 
   api.createPages(async ({ graphql, createPage }) => {
-    const { data } = await graphql(`{
-      allBlog {
-        edges {
-          node {
-            id
-            path
-          }
-          next {
-            id
-          }
-          previous {
-            id
+    const { data } = await graphql(`
+      {
+        allBlog {
+          edges {
+            node {
+              id
+              path
+            }
+            next {
+              id
+            }
+            previous {
+              id
+            }
           }
         }
       }
-    }`)
+    `)
 
     data.allBlog.edges.forEach(element => {
       createPage({
         path: element.node.path,
         component: './src/templates/Post.vue',
         context: {
-          previousId: (element.previous) ? element.previous.id : '#',
-          nextId: (element.next) ? element.next.id : '#',
-          id: element.node.id
-        }
+          previousId: element.previous ? element.previous.id : '#',
+          nextId: element.next ? element.next.id : '#',
+          id: element.node.id,
+        },
       })
     })
   })
 
   api.beforeBuild(context => {
-    const collection = context._app.store.getCollection('Blog')._collection
-
-    const posts = collection.data.map(post => {
-      return {
-        title: post.title,
-        path: post.path,
-        tags: post.tags
-      }
-    })
-
-    writeToFile('search index', paths.search, posts)
+    const posts = context._app.store
+      .getCollection('Blog')
+      ._collection.data.map(post => (({ title, path, tags }) => ({ title, path, tags }))(post))
+    createSearchIndex(posts, 'static/search.json')
   })
 }
