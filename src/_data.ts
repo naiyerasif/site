602315b10ba9dd6fc6ec77740cb2e9114c71952f
Naiyer Asif <19614213+naiyerasif@users.noreply.github.com>
2022-07-02 19:34:34 +0530

    setup: configure shared data

---
+++ src/_data.ts
@@ -0,0 +1,20 @@
+export default {
+	layout: 'default.tmpl.js',
+	app: {
+		version: '0.16.0',
+		title: 'Microflash',
+		description: 'Reflections on design and development by Naiyer Asif',
+		author: 'Naiyer Asif',
+		repository: 'https://github.com/Microflash/site.git',
+		url: 'https://mflash.dev',
+		networks: [
+			{ id: 'twitter', url: 'https://twitter.com/Microflash', username: '@Microflash' },
+			{ id: 'github', url: 'https://github.com/naiyerasif', username: '@naiyerasif' },
+			{ id: 'linkedin', url: 'https://in.linkedin.com/in/naiyerasif', username: 'naiyerasif' },
+		],
+		limits: {
+			outdation: 1, // in years
+			feedItems: 20
+		}
+	}
+}
