808200337747d16d60a8ad99363cd0588d91af79
Naiyer Asif <19614213+naiyerasif@users.noreply.github.com>
2022-07-02 19:35:27 +0530

    setup: config server port and site location

--- _config.ts
+++ _config.ts
@@ -1,8 +1,13 @@
 import lume from 'lume/mod.ts'
+import data from './src/_data.ts'
 
 const site = lume({
 	src: './src',
 	dest: './dist',
+	location: new URL(data.app.url),
+	server: {
+		port: 8080
+	}
 })
 
 export default site
