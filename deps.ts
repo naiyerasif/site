0766b170c8ea11a78bedf87222e9cd50fe534759
Naiyer Asif <19614213+naiyerasif@users.noreply.github.com>
2022-07-02 21:15:27 +0530

    feat(mod): add slugify module

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
 export { default as dayjs } from 'https://esm.sh/dayjs@1.11.3'
 export { default as timezone } from 'https://esm.sh/v86/dayjs@1.11.3/plugin/timezone'
 export { default as utc } from 'https://esm.sh/v86/dayjs@1.11.3/plugin/utc'
+export * as slugify from 'https://esm.sh/@sindresorhus/slugify@2.1.0'
 
+export type { Options as SlugifyOptions } from 'https://esm.sh/@sindresorhus/slugify@2.1.0'
