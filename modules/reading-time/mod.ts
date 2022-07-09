7a1f8633e5d24da391ed2eae3911689c51df2d3e
Naiyer Asif <19614213+naiyerasif@users.noreply.github.com>
2022-07-09 18:49:30 +0530

    refactor(mod): reading time options

--- modules/reading-time/mod.ts
+++ modules/reading-time/mod.ts
@@ -1,16 +1,16 @@
 import { merge } from 'lume/core/utils.ts'
 
-interface ReadingTimeOptions {
+export interface Options {
 	/** Reading speed in words per minute */
 	wpm: number
 }
 
-export const defaults: ReadingTimeOptions = {
+export const defaults: Options = {
 	wpm: 250
 }
 
 /** Returns an estimate of reading time in minutes */
-export default function readingTime(text: string, userOptions?: Partial<ReadingTimeOptions>): number {
+export default function readingTime(text: string, userOptions?: Partial<Options>): number {
 	const options = merge(defaults, userOptions)
 
 	const wordCount = text.trim().split(/\s+/g).length
