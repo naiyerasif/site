d85b1fa2b9b130a33e119fc1b1b577d3c16a5dac
Naiyer Asif <19614213+naiyerasif@users.noreply.github.com>
2022-07-09 19:17:59 +0530

    feat(template): posts

---
+++ src/posts.tmpl.js
@@ -0,0 +1,14 @@
+export const layout = 'layouts/posts.tmpl.js'
+
+export default function* ({ search, paginate }) {
+	const posts = search.pages('type=post', 'date=desc')
+	const options = {
+		url: (n) => n == 1 ? `/posts/` : `/posts/${n}/`,
+		size: 10
+	}
+
+	for (const page of paginate(posts, options)) {
+		page.type = 'posts'
+		yield page
+	}
+}
