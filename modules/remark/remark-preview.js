036b23de38d04db6eee5886b321cf4f371c2e8b2
Naiyer Asif <19614213+naiyerasif@users.noreply.github.com>
2022-07-15 09:45:32 +0530

    feat(mod): faster preview implementation

--- modules/remark/remark-preview.js
+++ modules/remark/remark-preview.js
@@ -1,20 +1,24 @@
 import { unified, remarkParse, unistUtilVisit, nlcstToString, hastscript } from '../../deps.ts'
 
-// @ts-ignore: Remark-provided types should fix the type conflict
 const parser = unified.unified().use(remarkParse)
 
-const previews = [
-	{
-		type: 'youtube',
-		generate: (id, caption) => {
-			let template = `<div class="preview-iframe-container"><iframe class="preview-iframe" src="https://www.youtube.com/embed/${id}" allow="join-ad-interest-group 'none'; run-ad-auction 'none'; encrypted-media; picture-in-picture; fullscreen"></iframe></div>`
+const previews = {
+	youtube: {
+		check(attributes) {
+			let validated = true
+			if (!attributes.id) {
+				console.warn('Missing youtube video id', attributes)
+				validated = false
+			}
+			return validated
+		},
+		template(attributes, caption) {
+			let template = `<div class="preview-iframe-container"><iframe class="preview-iframe" src="https://www.youtube.com/embed/${attributes.id}" allow="join-ad-interest-group 'none'; run-ad-auction 'none'; encrypted-media; picture-in-picture; fullscreen"></iframe></div>`
 			if (caption) template += `<figcaption>${caption}</figcaption>`
 			return parser.parse(template).children
 		}
 	}
-]
-
-const previewTypes = previews.map(preview => preview.type)
+}
 
 export default function remarkPreview() {
 	return (tree) => {
@@ -23,22 +27,21 @@ export default function remarkPreview() {
 				node.type === 'leafDirective' ||
 				node.type === 'containerDirective'
 			) {
-				if (!previewTypes.includes(node.name)) return
+				if (!previews[node.name]) return
 
 				const data = node.data || (node.data = {})
 				const attributes = node.attributes || {}
+				const selected = previews[node.name]
 
-				if (!attributes.id) file.fail('Missing youtube video id', node)
-
-				const tagName = 'figure'
-				const selectedPreview = previews.find(preview => preview.type === node.name)
+				if (!selected.check(attributes)) return
 
 				node.attributes = {
-					class: `preview preview-${selectedPreview.type}`
+					class: `preview preview-${node.name}`
 				}
 
-				node.children = selectedPreview.generate(attributes.id, node.type === 'containerDirective' ? nlcstToString.toString(node.children) : null)
+				node.children = selected.template(attributes, node.type === 'containerDirective' ? nlcstToString.toString(node.children) : null)
 
+				const tagName = selected.tagName || 'figure'
 				data.hName = tagName
 				data.hProperties = hastscript.h(tagName, node.attributes).properties
 			}
