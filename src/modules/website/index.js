8d669ad4ffb0d409735b19a3b0bb85a44e8e13e7
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2023-06-24 16:28:18 +0000

    feat(mods): add website module

---
+++ src/modules/website/index.js
@@ -0,0 +1,34 @@
+const authorInfo = {
+	name: "Naiyer Asif",
+	networks: [
+		{ platform: "mastodon", id: "@naiyer", link: "https://mastodon.design/@naiyer" },
+		{ platform: "github", id: "@naiyerasif", link: "https://github.com/naiyerasif" },
+		{ platform: "linkedin", id: "naiyerasif", link: "https://in.linkedin.com/in/naiyerasif" },
+	]
+};
+
+const siteInfo = {
+	version: "4.0.0",
+	title: authorInfo.name,
+	description: `Personal space of ${authorInfo.name} on the web`,
+	author: authorInfo,
+	repository: "https://github.com/naiyerasif/site.git",
+	siteBase: "https://naiyerasif.com",
+	editBase: "https://github.com/naiyerasif/site/edit/main",
+	maxFeedItems: 20,
+	maxTocDepth: 3
+};
+
+function fullLink(path, base = siteInfo.siteBase) {
+	return new URL(path, base).href;
+}
+
+function editLink(path) {
+	return fullLink(path, siteInfo.editBase);
+}
+
+export {
+	siteInfo as default,
+	fullLink,
+	editLink
+};
