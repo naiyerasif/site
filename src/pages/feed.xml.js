77b931c462284edc94058c84de511f31593a2d94
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2023-07-07 23:09:52 +0530

    feat(route): rss feeds

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,43 @@
+import { getCollection } from "astro:content";
+import rss from "~astro-rss";
+import { compare } from "~datetime";
+import siteInfo, { fullLink, postPathname } from "~website";
+
+const baseUrl = siteInfo.siteBase;
+const author = siteInfo.author.name;
+const aboutUrl = fullLink("/about");
+
+const options = {
+	id: baseUrl,
+	title: siteInfo.title,
+	link: baseUrl,
+	description: siteInfo.description,
+	copyright: `2018, ${author}`,
+	feedLinks: {
+		rss: fullLink("/feed.xml")
+	}
+};
+
+export async function get() {
+	const posts = (await getCollection("post"))
+		.filter(post => post.data.category !== "status")
+		.sort((p1, p2) => compare(p1.data.date, p2.data.date))
+		.slice(0, siteInfo.maxFeedItems)
+		.map(post => {
+			const pageUrl = fullLink(postPathname(post.slug));
+			return {
+				title: post.data.title,
+				date: new Date(post.data.date),
+				author: [{
+					name: author,
+					email: author,
+					link: aboutUrl
+				}],
+				content: post.body,
+				link: pageUrl,
+				id: pageUrl
+			}
+		});
+	
+	return rss(posts, options);
+}
