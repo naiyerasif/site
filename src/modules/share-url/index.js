4da89ca422b2da950cd56d500da5104884a377d4
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2024-08-03 13:06:09 +0530

    fix(component): move dom ops in connectedCallback for share-url

--- src/modules/share-url/index.js
+++ src/modules/share-url/index.js
@@ -1,17 +1,22 @@
 // source: https://github.com/NigelOToole/share-url/blob/89309ed6de9a4e7ee284177f1291b7160c5a6d8d/src/scripts/share-url-wc.js#L1
 export class ShareUrl extends HTMLElement {
+	static register(tagName = "share-url") {
+		if ("customElements" in window) {
+			customElements.define(tagName, ShareUrl);
+		}
+	}
 
 	// Utilities
 	checkBoolean(string) {
 		if (string.toLowerCase() === "true") return true;
 		if (string.toLowerCase() === "false") return false;
 		return string;
-	};
+	}
 
 	camelCase(name, delim = "-") {
 		const pattern = new RegExp((delim + "([a-z])"), "g");
 		return name.replace(pattern, (match, capture) => capture.toUpperCase());
-	};
+	}
 
 	// Methods
 	constructor() {
@@ -28,7 +33,9 @@ export class ShareUrl extends HTMLElement {
 			textSuccess: "Shared",
 			maintainSize: false
 		};
+	}
 
+	connectedCallback() {
 		for (const item of this.getAttributeNames()) {
 			let prop = this.camelCase(item);
 			let value = this.checkBoolean(this.getAttribute(item));
@@ -36,22 +43,18 @@ export class ShareUrl extends HTMLElement {
 		}
 
 		this.button = this.querySelector("button");
-
 		this.textElement = this.querySelector(this.options.textSelector);
 		if (this.textElement === null) this.textElement = this.button;
 		if (this.options.textLabel) this.textElement.innerText = this.options.textLabel;
-	};
 
-	connectedCallback() {
 		if (navigator[this.options.action]) {
-			this.button.addEventListener("click", () => this.shareEvent());
+			this.button.addEventListener("click", () => this._shareEvent());
 		} else {
-			// console.log(this, this.button, this.options.action);
 			this.style.display = "none";
 		}
 	}
 
-	async shareEvent() {
+	async _shareEvent() {
 		try {
 			if (this.options.action === "share") {
 				await navigator.share({ title: this.options.title, text: this.options.title, url: this.options.url });
@@ -61,15 +64,11 @@ export class ShareUrl extends HTMLElement {
 				await navigator.clipboard.writeText(this.options.url);
 			}
 
-			// let textWidth = this.textElement.offsetWidth;
-			// this.textElement.innerText = this.options.textSuccess;
-			// if (this.options.maintainSize) this.textElement.style.width = `${Math.max(textWidth, this.textElement.offsetWidth)}px`;
 			this.button.classList.add(this.options.activeClass);
-		}
-		catch (error) {
+		} catch (error) {
 			if (error.name !== "AbortError") console.error(error.name, error.message);
 		}
 	}
 }
 
-customElements.define("share-url", ShareUrl);
+ShareUrl.register();
