cf6ad9ca3dca1e0362837d68e2089a4c05ff2772
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2024-09-02 23:16:59 +0530

    fix(rss): fix the image URLs in RSS feeds

---
+++ etc/bin/fixFeedImageUrls.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import { parse, join } from "path";
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import * as cheerio from "cheerio";
+
+const baseUrl = "www.naiyerasif.com";
+const dist = join(process.cwd(), "dist");
+const asset = join(dist, "_astro");
+
+const imageRegistry = readdirSync(asset)
+	.map(fileName => parse(fileName))
+	.filter(fileName => fileName.ext === ".webp" || fileName.ext === ".svg")
+	.map(fileName => ({
+		key: `${baseUrl}/images/${fileName.name.split(".")[0]}`,
+		value: `${baseUrl}/_astro/${fileName.base}`
+	}))
+	.reduce((a, v) => Object.assign(a, { [v.key]: v.value }), {});
+
+function fixImageUrls(feedFileName) {
+	let feed = readFileSync(join(dist, feedFileName), "utf-8");
+	const $ = cheerio.load(feed, {}, false);
+	$("img").each((index, image) => {
+		const { dir, name, ext } = parse(image.attribs.src);
+		const key = `${dir}/${name}`.replace("https://", "");
+		const imageSrc = imageRegistry[key];
+		feed = feed.replace(key + ext, imageSrc);
+	});
+	writeFileSync(join(dist, feedFileName), feed);
+}
+
+fixImageUrls("all.xml");
+fixImageUrls("feed.xml");
