3af06fa48e7b41caada23f43e342c94c31a83299
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2024-12-07 19:41:03 +0530

    feat(etc): palette generator for site

---
+++ etc/plumage/api.js
@@ -0,0 +1,45 @@
+import { TonalPalette, argbFromHex, hexFromArgb, themeFromSourceColor } from "@material/material-color-utilities";
+
+// APIs to generate color palettes based on Material 3
+
+const selectedTones = [5, 7, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 95, 98, 99];
+
+function paletteFromArgb(argb) {
+	const tones = TonalPalette.fromInt(argb);
+	return Object.fromEntries(selectedTones.map(tone => [tone, hexFromArgb(tones.tone(tone))]));
+}
+
+function paletteFromHex(hex) {
+	return paletteFromArgb(argbFromHex(hex));
+}
+
+function standalonePalette(colors) {
+	return Object.entries(colors)
+		.map(([name, value]) => ({
+			name: name,
+			input: value,
+			base: value,
+			palette: paletteFromHex(value)
+		}));
+}
+
+function blendedPalette(base, colors) {
+	const themeColors = Object.entries(colors)
+		.map(([name, color]) => ({
+			name: name,
+			value: argbFromHex(color),
+			blend: true
+		}));
+	const theme = themeFromSourceColor(argbFromHex(base), themeColors);
+	return theme.customColors.map(color => ({
+		name: color.color.name,
+		input: colors[color.color.name],
+		base: hexFromArgb(color.value),
+		palette: paletteFromArgb(color.value)
+	}));
+}
+
+export {
+	standalonePalette,
+	blendedPalette
+};
---
+++ etc/plumage/index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+// Generates color palettes based on Material 3
+// Usage: node ./etc/plumage/index.js
+
+import { writeFile } from "fs/promises";
+import { blendedPalette, standalonePalette } from "./api.js";
+
+const colors = {
+	primary: "#02805a",
+	secondary: "#0d110e",
+	neutral: "#ffffff",
+	commend: "#2abf1d",
+	deter: "#db0b2f",
+	warn: "#eb993e",
+	assert: "#318ce7"
+};
+
+const themes = {
+	light: "#fbfefb",
+	dark: "#0d110e"
+};
+
+function cssProperties(palette, prefix = "color") {
+	return ":root {\n" +
+		palette.map(item => Object.entries(item.palette)
+			.map(([tone, color]) => `\t--${prefix}-${item.name}-${tone}: ${color};`)
+			.join("\n")
+		).join("\n\n") +
+		"\n}";
+}
+
+function scssProperties(palette, prefix = "color") {
+	return palette.map(item => Object.entries(item.palette)
+		.map(([tone, color]) => `$${prefix}-${item.name}-${tone}: ${color};`)
+		.join("\n")
+	).join("\n\n");
+}
+
+async function writeThemeFile(fileName, content) {
+	try {
+		await writeFile(fileName, content);
+		console.log(`Output written to ${fileName}`);
+	} catch (err) {
+		console.error("Error writing file:", err);
+	}
+};
+
+writeThemeFile("./src/styles/_colors.scss", scssProperties(standalonePalette(colors)));
