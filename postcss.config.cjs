8f155aceb300aaf92e9e8d6c80eaa8e753739b0d
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2025-01-03 22:53:21 +0530

    feat(perf): cut down useless CSS from the bundle

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,17 +3,8 @@ const postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
 	plugins: [
 		postcssPresetEnv({
+			stage: 2,
 			preserve: true,
-			enableClientSidePolyfills: true,
-			features: {
-				"focus-visible-pseudo-class": true,
-				"focus-within-pseudo-class": true,
-				"is-pseudo-class": true,
-				"logical-overflow": true,
-				"logical-overscroll-behavior": true,
-				"logical-properties-and-values": true,
-				"media-query-ranges": true,
-			}
 		}),
 	]
 };
