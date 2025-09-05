1bb44ea11c67958c6b766cd021ef78f5bb00c8cf
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2025-09-05 16:59:33 +0530

    feat(mod): improve share-url registration and teardown

--- src/modules/share-url/index.js
+++ src/modules/share-url/index.js
@@ -1,24 +1,11 @@
 // source: https://github.com/NigelOToole/share-url/blob/89309ed6de9a4e7ee284177f1291b7160c5a6d8d/src/scripts/share-url-wc.js#L1
 export class ShareUrl extends HTMLElement {
 	static register(tagName = "share-url") {
-		if ("customElements" in window) {
-			customElements.define(tagName, ShareUrl);
+		if ("customElements" in window && !customElements.get(tagName)) {
+			customElements.define(tagName, this);
 		}
 	}
 
-	// Utilities
-	checkBoolean(string) {
-		if (string.toLowerCase() === "true") return true;
-		if (string.toLowerCase() === "false") return false;
-		return string;
-	}
-
-	camelCase(name, delim = "-") {
-		const pattern = new RegExp((delim + "([a-z])"), "g");
-		return name.replace(pattern, (match, capture) => capture.toUpperCase());
-	}
-
-	// Methods
 	constructor() {
 		super();
 
@@ -30,15 +17,14 @@ export class ShareUrl extends HTMLElement {
 			title: document.title,
 			textSelector: null,
 			textLabel: "",
-			textSuccess: "Shared",
-			maintainSize: false
+			textSuccess: "Shared"
 		};
 	}
 
 	connectedCallback() {
 		for (const item of this.getAttributeNames()) {
-			let prop = this.camelCase(item);
-			let value = this.checkBoolean(this.getAttribute(item));
+			let prop = this._camelCase(item);
+			let value = this._checkBoolean(this.getAttribute(item));
 			this.options[prop] = value;
 		}
 
@@ -48,9 +34,16 @@ export class ShareUrl extends HTMLElement {
 		if (this.options.textLabel) this.textElement.innerText = this.options.textLabel;
 
 		if (navigator[this.options.action]) {
-			this.button.addEventListener("click", () => this._shareEvent());
+			const { signal } = this.abortController = new AbortController();
+			this.button.addEventListener("click", () => this._shareEvent(), { signal });
 		} else {
-			this.style.display = "none";
+			this.hidden = true;
+		}
+	}
+
+	disconnectedCallback() {
+		if (this.abortController) {
+			this.abortController.abort();
 		}
 	}
 
@@ -68,6 +61,17 @@ export class ShareUrl extends HTMLElement {
 		} catch (error) {
 			if (error.name !== "AbortError") console.error(error.name, error.message);
 		}
+	}
+
+	_checkBoolean(string) {
+		if (string.toLowerCase() === "true") return true;
+		if (string.toLowerCase() === "false") return false;
+		return string;
+	}
+
+	_camelCase(name, delim = "-") {
+		const pattern = new RegExp((delim + "([a-z])"), "g");
+		return name.replace(pattern, (match, capture) => capture.toUpperCase());
 	}
 }
 
