b9e06684e2bcbcb3f75b1efb848dd1d20f2b3fe8
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2026-06-14 16:43:00 +0530

    markdown: switch to published description list directive

--- astro.config.js
+++ astro.config.js
@@ -12,10 +12,10 @@ import {
 	languageIndicatorPlugin,
 	lineAnnotationPlugin
 } from "@microflash/rehype-starry-night/plugins";
+import { remarkDescriptionListDirective } from "@naiyer/remark-directives";
 import remarkTimeDirective from "#mods/remark-time-directive/index.js";
 import remarkFigureDirective from "#mods/remark-figure-directive/index.js";
 import remarkYoutubeDirective from "#mods/remark-youtube-directive/index.js";
-import remarkDescriptionListDirective from "#mods/remark-desclist-directive/index.js";
 import siteInfo, { calloutOptions } from "./src/modules/website/index.js";
 import { CountableSlugifier } from "./src/modules/slugifier/index.js";
 import sourcePgsql from "./src/modules/textmate/source.pgsql.js";
